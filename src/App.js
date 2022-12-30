88cc81f5143fc4a268839bc7ffb8e6857870f44f
NguyenDacHoangAnh <anh.ndh204510@sis.hust.edu.vn>
2022-12-30 10:47:15 +0700

    find && filter && detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import AllCountries from './components/AllCountries/AllCountries';
+import CountryInfo from './components/CountryInfo/CountryInfo';
 
 function App() {
   return (
     <>
-      <div>
+      <div className='header'>
+        <div className='container'>
+          <h5>Where in the world</h5>
+        </div>
+      </div>
 
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<AllCountries />}></Route>
+          <Route path='/country/:countryName' element={<CountryInfo />}></Route>
+        </Routes>
       </div>
     </>
   );
